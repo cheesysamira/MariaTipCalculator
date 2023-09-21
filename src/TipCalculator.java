8c5af1e7b33eac18d25e747675f7dead4c0812d3
BT_1N3_11 <sample@bths.edu>
2023-09-21 10:18:41 -0400

    Initialize commit

---
+++ src/TipCalculator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class TipCalculator {
+    public static void main(String[] args){
+        System.out.println("Welcome to the tip calculator!");
+
+        System.out.println("How many people are in your group: ");
+        int peopleInGroup = scan.nextInt();
+        double cost;
+
+        while (cost != -1.00){
+
+        }
+    }
+}
