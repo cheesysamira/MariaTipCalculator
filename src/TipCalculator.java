4bf9db1eb6158c49ba921bdf8a726956026d50ca
BT_1N3_11 <sample@bths.edu>
2023-09-21 10:33:36 -0400

    Added list for items!

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class TipCalculator {
     public static void main(String[] args){
         System.out.println("Welcome to the tip calculator!");
@@ -9,11 +11,17 @@ public class TipCalculator {
         System.out.println("What's the tip percentage? (0-100): ");
         int tipPercentage = scan.nextInt();
 
-
         double cost;
+        double itemCost;
 
+        ArrayList<String> items = new ArrayList<String>();
         while (cost != -1.00){
-
+            System.out.println("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
+            itemCost = scan.nextDouble();
+            cost += itemCost;
+            System.out.println("Enter the item: ");
+            String item = scan.nextLine();
+            items.add(item);
         }
     }
 }
